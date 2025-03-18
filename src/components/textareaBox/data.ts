991e4a13b543200def012e207966784974ab66ee
KimJaeHee <jaiyah@naver.com>
2025-03-18 18:02:20 +0900

    Init textareaBox

---
+++ src/components/textareaBox/data.ts
@@ -0,0 +1,30 @@
+const data = [
+  {
+    id: '67d27b2fbbb20a94cdd9b1c4',
+    title: 'Eiusmod do magna deserunt labore voluptate in officia.',
+    description:
+      'Aliqua consectetur commodo officia incididunt esse esse. Voluptate consectetur qui qui veniam tempor aliqua qui mollit labore minim irure quis. ',
+  },
+  {
+    id: '67d27b2f4dff8046fbc2f6e7',
+    title: 'Aliqua culpa et consequat elit.',
+    description:
+      'Laborum incididunt excepteur occaecat minim elit. Dolore proident occaecat minim reprehenderit fugiat proident minim dolor. Aliqua officia aliqua sunt exercitation id laboris magna consequat nostrud minim. ',
+  },
+  {
+    id: '67d27b2f62c05bc80cfd8de6',
+    title:
+      'Id tempor enim pariatur id do occaecat sit consequat pariatur. Sit qui culpa minim minim occaecat cillum ea velit.',
+    description:
+      'Pariatur labore irure irure ut excepteur. Sint voluptate ut incididunt minim culpa eiusmod nulla eiusmod cupidatat duis veniam. ',
+  },
+  {
+    id: '67d27b2f7ded408432fa4594',
+    title:
+      'Ipsum ullamco commodo pariatur duis officia voluptate cupidatat minim duis. Est ex sunt excepteur mollit dolor velit sint laboris Lorem occaecat aute.',
+    description:
+      'Do ad cupidatat excepteur quis eu mollit eiusmod exercitation duis. Aliqua et tempor adipisicing elit in culpa veniam fugiat adipisicing. ',
+  },
+];
+
+export default data;
